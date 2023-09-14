f2b130c0d0d0e780ad4b2f1294781eda640edc26
yogitashete2412 <yogitashete24@gmail.com>
2023-09-14 13:19:56 +0530

    navbar created

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
