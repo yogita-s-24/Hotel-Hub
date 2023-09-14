f73a1ff96ee5e992cd0d7c3e1c5c0623cfdc5438
yogitashete2412 <yogitashete24@gmail.com>
2023-09-14 10:17:33 +0530

    created folder structure

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <>
+ <Navbar/>
+ <Footer/>
+ 
+ </>
+);
+
+
